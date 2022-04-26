bd5ab8c08b46d2c844b8b3d62528f8557bbefd6b
Holden Gutwin <96965385+holdengutwin@users.noreply.github.com>
2022-04-25 20:51:03 -0600

    Minor padding and context fixes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,9 +7,9 @@ const Home: NextPage = () => {
   let linkedinLink = <a href="https://linkedin.com/in/holdengutwin" style={{color:"blue"}}>Linkedin</a>
   let githubLink = <a href="https://github.com/holdengutwin" style={{color:"blue"}}> GitHub</a>
   return (
-    <div style={{paddingLeft:"30px"}}>
+    <div style={{padding:"0 30px"}}>
       <h1>{`Hi! I'm Holden Gutwin 😄`}</h1>
-      <p>{`I'm a third year computer science student at the University of Saskatchewan, currently on internship at Andgo Systems.`}</p>
+      <p>{`I'm a third year computer science student at the University of Saskatchewan, starting an internship at Andgo Systems in May.`}</p>
       <p>This page is still under construction, feel free to check out my {githubLink} and {linkedinLink} in the meantime!</p>
     </div>
   )
